02762a5adac2c0684a7fa5f4148be015adf9dade
A2Abdi <alifeki45@gmail.com>
2023-06-22 12:37:58 -0400

    final

--- DrawIO'.py
+++ DrawIO'.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import grad_descent, predictions
+from pynput import keyboard
+import random
 
 data = pd.read_csv('train.csv')
 data = np.array(data)
@@ -20,4 +22,11 @@ y_dev = data_dev[0]
 x_dev = data_dev[1:col]
 x_dev = x_dev / 255
 
-W1, b1 , W2 , b2 = grad_descent.gradient_descent(x_train, y_train , 1000, 0.25, row)
+W1, b1 , W2 , b2 = grad_descent.gradient_descent(x_train, y_train , 50, 0.35, row)
+
+a = input("click space to get out")
+
+while a != keyboard.Key.space:
+    b = random.randint(0,9)
+    predictions.test_predictions(b, W1, b1, W2 , b2, x_train, y_train)
+    a
--- predictions.py
+++ predictions.py
@@ -3,7 +3,7 @@ import forward_prop, grad_descent
 
 
 def make_prediction(X, W1, b1, W2, b2 ):
-    _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+    _, _, _, A2 = forward_prop.forward_prop(W1, b1, W2, b2, X)
     predictions = grad_descent.get_prediction(A2)
     return predictions
 def test_predictions(index, W1, b1, W2, b2, x_train, y_train):
